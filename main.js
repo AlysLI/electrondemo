860bb8e8aa68780ee20a5495214f64690288c5f7
AlysLI <71476778+AlysLI@users.noreply.github.com>
2020-11-11 18:09:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,66 @@
-/* 一、electron運行流程
-   1.读取package.json的中的入口文件,这里我们是main.js
-   2.main.js 主进程中创建渲染进程
-   3.读取应用页面的布局和样式
-   4.使用IPC在主进程执行任务并获取信息 
-   二、node里的fs模块
-   1.包括文件目录的创建、删除、查询以及文件的读取和写入*/
+'use strict' //启用严格模式
 
 var electron = require('electron')
 var app = electron.app
 var BrowserWindow = electron.BrowserWindow
-const path = require('path')
-const url = require('url')
-const ipc = require('electron').ipcMain // 跳转主进程,使用IPC在主进程执行任务并获取信息
-const fs = require("fs")
-var mainWindow = null //主窗口
-app.on('ready', () => {
-    //創建主窗口
-    mainWindow = new BrowserWindow({
-        width: 360,
-        height: 300,
-        resizable: false, // 禁止改变主窗口尺寸
-        webPreferences: {
-            nodeIntegration: true, // 解决在渲染层的其他js文件中出现require无法使用的情况【node的方式引入模块】   
-            enableRemoteModule: true //新版的electron由於安全原因默認關閉了remote模塊，需要在主進程——main.js的webPreferences配置enableRemoteModule:true
-        }
-    })
-    /* //創建菜單 */
-    //require('./scripts/js/menu.js')
-    //加載本地頁面到應用中
-    mainWindow.loadFile('index.html')
-    //mainWindow.setMenu(null);                      // 关闭菜单栏
-    //mainWindow.webContents.openDevTools(); //F12調試
-    //監聽主窗口關閉事件
-    mainWindow.on('closed', () => {
-        mainWindow = null
-    })
+var fs = require('fs')
+var path = require('path')
+var { ipcMain } = require('electron')
+//var discontent = document.getElementById('content')
+// var isMac = process.platform === 'darwin'
+// var isWindows = process.platform === 'win32'
+//tally點檢窗口
+let tally = null
+let loginWin
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 560,
+    height: 810,
+    center: false,
+    webPreferences: {
+      nodeIntegration: true, // 解决在渲染层的其他js文件中出现require无法使用的情况【node的方式引入模块】
+      enableRemoteModule: true, //新版的electron由於安全原因默認關閉了remote模塊，需要在主進程——main.js的webPreferences配置enableRemoteModule:true
+      //preload: path.join(__dirname, 'login.js'),
+    },
+  })
+  mainWindow.loadFile('index.html')
+  mainWindow.webContents.openDevTools()
+
+  loginWin = new BrowserWindow({
+    width: 340,
+    height: 320,
+    resizable: false, // 禁止改变主窗口尺寸
+    webPreferences: { nodeIntegration: true },
+  })
+  loginWin.once('ready-to-show', () => {
+    loginWin.show()
+  })
+  loginWin.loadFile('login.html')
+  loginWin.setMenu(null) //關閉菜單欄
+  loginWin.webContents.openDevTools()
+  loginWin.on('closed', () => {
+    mainWindow = null
+  })
+}
+require('./src/render/menu.js')
+
+ipcMain.on('window-hide', () => {
+  loginWin.hide() //隱藏登錄窗口
+
+  //discontent.style.visibility = 'visible' //首頁内容顯示
+})
+
+app.on('ready', createWindow)
+
+/* 在最後一個窗口關閉時退出應用 */
+app.on('window-all-closed', function () {
+  app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
 })
